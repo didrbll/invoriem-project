917acfa9bccf47af65788aff98c96fadf25742d8
didrbll <st-antonova.e@rggu.ru>
2023-07-16 20:06:08 +0300

    do video section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ $(document).ready(function () {
     loop: true,
   });
 });
+
+const videoBtn = document.querySelector("#video-btn");
+const videoImg = document.querySelector(".video__img");
+const videoWrapper = document.querySelector(".video");
+const video = document.querySelector("#video-obj");
+
+videoWrapper.addEventListener("click", function () {
+  if (video.paused) {
+    videoImg.classList.add("none");
+    videoWrapper.classList.remove("video--overlay");
+    videoBtn.classList.add("none");
+    video.play();
+  } else {
+    videoImg.classList.remove("none");
+    videoWrapper.classList.add("video--overlay");
+    videoBtn.classList.remove("none");
+    video.pause();
+  }
+});
